87ddf68e68e21243e0c3950832fdb7424acac8e7
fawgv <fawgv@mail.ru>
2018-11-25 00:47:47 +0500

    Init

---
+++ Code Retreat/GameArea.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace Code_Retreat
+{
+    public class GameArea
+    {
+        public HashSet<Point> AlivePoints = new HashSet<Point>();
+        private HashSet<Point> AllNeighboursPoints = new HashSet<Point>();
+
+        public GameArea(HashSet<Point> alivePoints)
+        {
+            AlivePoints = alivePoints;
+        }
+
+        public void NextStep()
+        {
+            FillAllNeighboursPoints();
+            HashSet<Point> newAlivePoints = new HashSet<Point>();
+            foreach (var point in AllNeighboursPoints)
+            {
+                var neighboursCount = GetAlivePointsInNeighbours(point);
+                if (VerifyIfAlive(neighboursCount, point))
+                {
+                    newAlivePoints.Add(point);
+                }
+            }
+
+            AlivePoints = newAlivePoints;
+        }
+
+        private bool VerifyIfAlive(int aliveNPointsCount, Point point)
+        {
+            if (aliveNPointsCount < 2)
+                return false;
+            else if (aliveNPointsCount > 3)
+                return false;
+            else if (aliveNPointsCount == 3)
+                return true;
+            return AlivePoints.Contains(point);
+        }
+
+        private void FillAllNeighboursPoints()
+        {
+            foreach (var alivePoint in AlivePoints)
+            {
+                for (int x = alivePoint.X - 1; x <= alivePoint.X + 1; x++)
+                {
+                    for (int y = alivePoint.Y - 1; y <= alivePoint.Y + 1; y++)
+                    {
+                        AllNeighboursPoints.Add(new Point(x, y));
+                    }
+                }
+            }
+        }
+
+        private int GetAlivePointsInNeighbours(Point point)
+        {
+            int result = 0;
+            var aliveWithoutPoint = AlivePoints.Where(point1 => point != point1).Select(point1 => point1);
+            for (int x = point.X - 1; x <= point.X + 1; x++)
+            {
+                for (int y = point.Y - 1; y <= point.Y + 1; y++)
+                {
+                    if (VerifyPointInAlivePoints(new Point(x, y),aliveWithoutPoint))
+                    {
+                        result++;
+                    }
+                }
+            }
+
+
+            return result;
+        }
+
+        private bool VerifyPointInAlivePoints(Point point, IEnumerable<Point> alivePoints)
+        {
+            bool result = false;
+            foreach (var alivePoint in alivePoints)
+            {
+                if (alivePoint.X == point.X && alivePoint.Y == point.Y)
+                {
+                    result = true;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Code Retreat/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace Code_Retreat
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var map = new Boolean[30, 30];
+            var alivePoints = new HashSet<Point>()
+            {
+                new Point(3,3),new Point(4,3),new Point(5,3),
+                                                            new Point(5,4),new Point(6,4),
+                new Point(3,5),new Point(4,5),new Point(5,5)
+            };
+            
+            var game = new GameArea(alivePoints);
+
+            while (true)
+            {
+                game.NextStep();
+
+                foreach (var point in game.AlivePoints)
+                {
+                    try
+                    {
+                        map[point.X, point.Y] = true;
+                    }
+                    catch
+                    {
+
+                    }
+                }
+
+                for (var x = 0; x < 30; x++)
+                {
+                    for (int y = 0; y < 30; y++)
+                    {
+                        Console.Write(map[x, y] == true ? "X" : " ");
+                    }
+                    Console.WriteLine();
+                }
+                    
+
+                //map = new bool[50,50];
+                
+                Thread.Sleep(100);
+                Console.Clear();
+            }
+        }
+    }
+
+    [TestFixture]
+    public class Test
+    {
+
+    }
+}
